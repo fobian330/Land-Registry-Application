0ec1cc356c8c0c5c900eb6c5fdc0df166b820c0b
Fabian Ouma <105557382+fobian330@users.noreply.github.com>
2025-03-13 12:31:49 +0000

    created server node moduls, added mongodb uri, installed mongodb via npm

--- Server/backend/Config/db_config.js
+++ Server/backend/Config/db_config.js
@@ -4,5 +4,5 @@ module.exports = {
   NEXMO_API_KEY: "API_KEY",
   NEXMO_API_SECRET: "SECRET_KEY",
   NEXMO_FROM_NUMBER: "NUMBER",
-  MongoURI: `ENTER_MONGOURI`
+  MongoURI: "mongodb+srv://fobianouma:af63&BBIT@landtarns.kaofi.mongodb.net/?retryWrites=true&w=majority&appName=LandTarns"
 }
